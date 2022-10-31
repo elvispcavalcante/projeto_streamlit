ec280633ca18ed0dc147612d87e69a0915e4084d
ElvisPC <104656917+elvispcavalcante@users.noreply.github.com>
2022-10-31 14:08:13 -0300

    Update gerar_users_senhas.py

--- gerar_users_senhas.py
+++ gerar_users_senhas.py
@@ -3,9 +3,9 @@ from pathlib import Path
 import streamlit_authenticator as stauth
 
 names = ['Marden', 'Gustavo', 'Elvis', 'Morato', 'Admin']
-usernames = ['marden', 'gustavo', 'elvis', 'morato', 'admin']
-passwords = ['12345abc', '12345abc', '12345abc', 'loja123456', 'Admin@*2022']
-# depois de criar o arquivo voltar aqui para ocultar a senha. Pode colocar XXX em cada uma delas
+usernames = ['marden', 'gustavo', 'elvis', 'admin']
+passwords = ['xxxxx', 'xxxxx', 'xxxxx', 'xxxxx',]
+
 
 hashed_passwords = stauth.Hasher(passwords).generate()
 
